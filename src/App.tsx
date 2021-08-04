bc89fdf551eeab29605f8174ed05fbb212ba4115
soojang <solitasroh@gmail.com>
2021-08-04 21:37:09 +0900

    초기 코드 커밋

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import styled from 'styled-components/native';
+
+import {SafeAreaView, Text} from 'react-native';
+import Todo from '~/Screens/Todo';
+
+const Container = styled.View`
+  flex: 1;
+  background-color: #fff;
+`;
+
+const Top = styled(SafeAreaView)`
+  flex: 1;
+`;
+
+const App = () => {
+  return (
+    <Top>
+      <Container>
+        <Todo />
+      </Container>
+    </Top>
+  );
+};
+
+export default App;
