6bc1c1b7dad561b7c7543dc42fc6e72cf4d957f9
soojang <solitasroh@gmail.com>
2021-08-05 22:41:34 +0900

    화원관리 컴포넌트 파일 초기코드 추가

---
+++ src/Screens/Login/Login.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+import styled from 'styled-components/native';
+
+const Login = () => {
+  return (
+    <View>
+      <Text>Login view</Text>
+    </View>
+  );
+};
+
+export default Login;
---
+++ src/Screens/Login/Welcome.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+import styled from 'styled-components/native';
+
+const Welcome = () => {
+  return (
+    <View>
+      <Text>Welcome view</Text>
+    </View>
+  );
+};
+
+export default Welcome;
