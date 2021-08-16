2752cc88d22ad59b3536115cd99b9068d31cda3f
soojang <solitasroh@gmail.com>
2021-08-16 22:03:47 +0900

    오류수정

--- src/Screens/Login/CreateAccount.tsx
+++ src/Screens/Login/CreateAccount.tsx
@@ -1,87 +1,103 @@
 import React from 'react';
 import {Text} from 'react-native';
 import styled from 'styled-components/native';
-import { StackNavigationProp } from '@react-navigation/stack';
+import {StackNavigationProp} from '@react-navigation/stack';
 import Input from './Input';
 import Button from './Button';
-import { LoginNaviParamList } from '../types';
-import { Controller, useForm } from 'react-hook-form';
+import {LoginNaviParamList} from '../types';
+import {Controller, useForm} from 'react-hook-form';
 
 const Container = styled.SafeAreaView`
-  flex : 1;
-  background-color : #FDF6F0;
+  flex: 1;
+  background-color: #fdf6f0;
   align-items: center;
   justify-content: center;
-  padding : 40px;
+  padding: 40px;
 `;
 
 type NavigationProp = StackNavigationProp<LoginNaviParamList, 'CreateAccount'>;
 
-interface Props { 
-  navigation :NavigationProp;
+interface Props {
+  navigation: NavigationProp;
 }
-const CreateAccount = ({navigation} : Props) => {
-  const { control, handleSubmit, formState: { errors } } = useForm({
+const CreateAccount = ({navigation}: Props) => {
+  const {
+    control,
+    handleSubmit,
+    formState: {errors},
+  } = useForm({
     defaultValues: {
       userID: '',
       userName: '',
-      userEmail:'',
-      userPW : ''
-    }
+      userEmail: '',
+      userPW: '',
+    },
   });
   const onSubmit = data => console.log(data);
-  const onError = (error) => { console.log(error)};
-  
+  const onError = error => {
+    console.log(error);
+  };
+
   return (
     <Container>
       <Controller
         control={control}
-        render={({ onChange, onBlur, value }) => (
-        <Input 
-        style = {{marginBottom : 16}} placeholder= "ID"  onChangeText={(text) => console.log({text})}
-        value={value}/>
-        
+        render={({onChange, onBlur, value}) => (
+          <Input
+            style={{marginBottom: 16}}
+            placeholder="ID"
+            onChangeText={onChange}
+            value={value}
+          />
         )}
-        name = "userID"        
-        rules={{ required: true }}
+        name="userID"
+        rules={{required: true}}
       />
       <Controller
         control={control}
-        render={({ onChange, onBlur, value }) => (
-        <Input 
-        style = {{marginBottom : 16}} placeholder= "Name"  onChangeText={value => onChangeText(value)}
-        value={value}/>
-        
+        render={({onChange, onBlur, value}) => (
+          <Input
+            style={{marginBottom: 16}}
+            placeholder="Name"
+            onChangeText={onChange}
+            value={value}
+          />
         )}
-        name = "userName"        
-        rules={{ required: true }}
+        name="userName"
+        rules={{required: true}}
       />
       <Controller
         control={control}
-        render={({ onChange, onBlur, value }) => (
-        <Input 
-        style = {{marginBottom : 16}} placeholder= "Password"  secureTextEntry ={true}
-        onChangeText={value => onChangeText(value)}  value={value}/>        
+        render={({onChange, onBlur, value}) => (
+          <Input
+            style={{marginBottom: 16}}
+            placeholder="Password"
+            secureTextEntry={true}
+            onChangeText={value => onChange}
+            value={value}
+          />
         )}
-        name = "userPW"        
-        rules={{ required: true }}
+        name="userPW"
+        rules={{required: true}}
       />
       <Controller
         control={control}
-        render={({ onChange, onBlur, value }) => (
-        <Input 
-        style = {{marginBottom : 16}} placeholder= "email" keyboardType="email-address"
-        onChangeText={value => onChangeText(value)}  value={value}/>        
+        render={({onChange, onBlur, value}) => (
+          <Input
+            style={{marginBottom: 16}}
+            placeholder="email"
+            keyboardType="email-address"
+            onChangeText={onChange}
+            value={value}
+          />
         )}
-        name = "userEmail"        
-        rules={{ required: true }}
+        name="userEmail"
+        rules={{required: true}}
       />
       <Button
-        style ={{marginBottom : 24}}
-        label = "Sign up"
-        onPress = {
-          handleSubmit(onSubmit, onError)
-        }
+        style={{marginBottom: 24}}
+        label="Sign up"
+        onPress={handleSubmit(onSubmit, onError)}
       />
     </Container>
   );
--- src/Screens/Login/Input.tsx
+++ src/Screens/Login/Input.tsx
@@ -2,51 +2,55 @@ import React from 'react';
 import styled from 'styled-components/native';
 
 const Container = styled.View`
-  width : 100%;
-  height : 40px;
-  padding-left : 16px;
-  padding-right : 16px;
-  border-radius : 4px;
-  background-color : #FFFFFF;
+  width: 100%;
+  height: 40px;
+  padding-left: 16px;
+  padding-right: 16px;
+  border-radius: 4px;
+  background-color: #ffffff;
 `;
 
 const InputField = styled.TextInput`
-    flex : 1;
-    color : #38738a;
-
+  flex: 1;
+  color: #38738a;
 `;
 
-interface Props{
-    placeholder ? : string;
-    keyboardType?: 'default' | 'email-address' | 'numeric' | 'phone-pad';
-    secureTextEntry?:boolean;
-    style?:Object;
-    clearMode ?: boolean;
-    onChangeText?:(text : string) => void;
+interface Props {
+  placeholder?: string;
+  keyboardType?: 'default' | 'email-address' | 'numeric' | 'phone-pad';
+  secureTextEntry?: boolean;
+  style?: Object;
+  clearMode?: boolean;
+  onChangeText?: (text: string) => void;
+  value: undefined;
 }
 
 const Input = ({
-    placeholder,
-    keyboardType,
-    secureTextEntry,
-    style,
-    clearMode,
-    onChangeText,} : Props) => {
-    return(
-        <Container style ={style}>
-            <InputField
-                selectionColor = "#38738a"
-                secureTextEntry = {secureTextEntry}
-                keyboardType ={keyboardType ? keyboardType : 'default'}
-                autoCapitalize = "none"
-                autoCorrect = {false}
-                allowFontScaling = {false}
-                placeholderTextColor = "#38738a"
-                placeholder = {placeholder}
-                clearButtonMode = {clearMode ? 'while-editing' : 'never'}
-                onChangeText = {onChangeText}/>
-        </Container>
-    );
+  placeholder,
+  keyboardType,
+  secureTextEntry,
+  style,
+  clearMode,
+  value,
+  onChangeText,
+}: Props) => {
+  return (
+    <Container style={style}>
+      <InputField
+        value={value}
+        selectionColor="#38738a"
+        secureTextEntry={secureTextEntry}
+        keyboardType={keyboardType ? keyboardType : 'default'}
+        autoCapitalize="none"
+        autoCorrect={false}
+        allowFontScaling={false}
+        placeholderTextColor="#38738a"
+        placeholder={placeholder}
+        clearButtonMode={clearMode ? 'while-editing' : 'never'}
+        onChangeText={onChangeText}
+      />
+    </Container>
+  );
 };
 
-    export default Input;
+export default Input;
